6d2ae03b57fd363a9a6cae92fd3108763e3a5e4a
Emerson <acessaprudente@gmail.com>
2023-10-06 18:20:47 -0300

    feeds

--- dataset.py
+++ dataset.py
@@ -1,26 +1,7 @@
-import json, requests
+import json
 import pandas as pd
-from datetime import date, datetime
-from pandas import json_normalize
 
 file = open('dados/feeds.json')
 data = json.load(file)
 df = pd.DataFrame.from_dict(data['feeds'])
 df['created_at'] = pd.to_datetime(df['created_at'], format="%Y/%m/%d %H:%M:%S", exact=False, dayfirst=True, errors='ignore')
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
