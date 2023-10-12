2e7818a45ef345ad4e9a8af83f16a11174faa256
acessaprudente <103765412+acessaprudente@users.noreply.github.com>
2023-10-12 19:29:09 -0300

    Update apresentacao.py

--- pages/apresentacao.py
+++ pages/apresentacao.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
 st.title('🌦️ Projeto Integrador V')
-components.iframe("https://docs.google.com/presentation/d/1AYOyHh_zOPG3w_8T8M_zZwFCgk34oik-1CCma1417VA/edit?usp=sharing", height=600)
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSAbxrn9Gk_Kk47HI8AORJ4HakRl21KPgQPyXDoEVJ6Tgx9ftiF_Biuu89GkFuoLOXw1w6nkhUcoVrJ/pub?start=false&loop=false&delayms=3000", height=600)
 
 
