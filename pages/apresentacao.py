56edbee931c39185ed3d6862b3d03a056ee2de2d
Emerson <acessaprudente@gmail.com>
2023-10-12 20:56:41 -0300

    teste2

--- pages/apresentacao.py
+++ pages/apresentacao.py
@@ -2,10 +2,3 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.title('🌦️ Projeto Integrador V')
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSAbxrn9Gk_Kk47HI8AORJ4HakRl21KPgQPyXDoEVJ6Tgx9ftiF_Biuu89GkFuoLOXw1w6nkhUcoVrJ/pub?start=false&loop=false&delayms=3000", height=600)
-<<<<<<< HEAD
-
-
-=======
->>>>>>> refs/remotes/origin/main
-
-
