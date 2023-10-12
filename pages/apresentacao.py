c3cfa913cc5f6614549dc7547726aa7d0cc444cc
Emerson <acessaprudente@gmail.com>
2023-10-12 20:40:27 -0300

    apresentacao2

--- pages/apresentacao.py
+++ pages/apresentacao.py
@@ -4,3 +4,5 @@ st.title('🌦️ Projeto Integrador V')
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vSAbxrn9Gk_Kk47HI8AORJ4HakRl21KPgQPyXDoEVJ6Tgx9ftiF_Biuu89GkFuoLOXw1w6nkhUcoVrJ/pub?start=false&loop=false&delayms=3000", height=600)
 
 
+
+
